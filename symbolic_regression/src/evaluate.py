395b74d41bea04acb70117be0365fca29be7c6b0
Stefano Roy Bisignano <bisiwork01@gmail.com>
2025-01-04 12:51:44 +0100

    remove unused files and functions related to symbolic regression

--- symbolic_regression/src/evaluate.py
+++
@@ -1,6 +0,0 @@
-from gxgp.gp_dag import DagGP
-
-def calculate_mse(model, x, y):
-    """Calcola il Mean Squared Error"""
-    y_pred = DagGP.evaluate(model, x.T)
-    return sum((y - y_pred) ** 2) / len(y)
